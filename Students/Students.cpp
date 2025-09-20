3d9d68cd3404717df625c38e2865898410cee3c6
YellovvFLash <171489175+YellovvFLash@users.noreply.github.com>
2025-09-20 19:53:37 +0200

    v0.1

--- Students/Students.cpp
+++ Students/Students.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <iomanip>
 
 using std::cout;
 using std::string;
@@ -16,15 +17,31 @@ class Studentas {
     string pavarde;
     vector<int> pazymiai;
     int egzaminas = 0;
-    double rezultatas = 0.0;
+    double rezultatasV = 0.0;
+    double rezultatasM = 0.0;
+
+    static bool naudotiMediana_;
+
 
     void vidurkis() {
         if (!pazymiai.empty()) {
             double avg = std::accumulate(pazymiai.begin(), pazymiai.end(), 0.0) / pazymiai.size();
-            rezultatas = avg * 0.4 + egzaminas * 0.6;
+            rezultatasV = avg * 0.4 + egzaminas * 0.6;
         }
         else {
-            rezultatas = egzaminas * 0.6; // jei ND nėra
+            rezultatasV = egzaminas * 0.6;
+        }
+    }
+    void mediana() {
+        if (!pazymiai.empty()) {
+            std::vector<int> t = pazymiai;
+            std::sort(t.begin(), t.end());
+            double med = (t.size() % 2 == 0) ? (t[t.size() / 2 - 1] + t[t.size() / 2]) / 2.0
+                : t[t.size() / 2];
+            rezultatasM = med * 0.4 + egzaminas * 0.6;
+        }
+        else {
+            rezultatasM = egzaminas * 0.6;
         }
     }
 
@@ -41,9 +58,21 @@ public:
     int getEgzaminas() const {
         return egzaminas;
     }
-    double getRezultatas() const {
-        return rezultatas;
+    double getRezultatasV() const {
+        return rezultatasV;
     }
+    double getRezultatasM() const {
+        return rezultatasM;
+    }
+    static void setModeMediana(bool m) { naudotiMediana_ = m; }  
+   
+    static void printHeader(std::ostream& out) {
+        out << std::left << std::setw(13) << "Vardas"
+            << std::setw(13) << "Pavarde"
+            << (naudotiMediana_ ? "Galutinis (Med.)" : "Galutinis (Vid.)") << "\n"
+            << "------------------------------------------------\n";
+    }
+
     // Default constructor
     Studentas() {
 
@@ -54,7 +83,9 @@ public:
         pazymiai = paz;
         egzaminas = egzas;
         vidurkis();
+        mediana();
     }
+
     // priskyrimo-kopijavimo operatorius
     Studentas& operator=(const Studentas& other) {
         if (this == &other) return *this; 
@@ -73,10 +104,10 @@ public:
         pavarde.clear();
         pazymiai.clear();
         egzaminas = 0;
-        rezultatas = 0;
+        rezultatasV = 0;
+        rezultatasM = 0;
     };
-    // operatorių perkrovimas
-    // 
+    // operatorių perkrovimas 
     friend istream& operator>>(istream& in, Studentas& s) {
         cout << "Iveskite varda ir pavarde: ";
         in >> s.vardas >> s.pavarde;
@@ -94,20 +125,31 @@ public:
         in >> s.egzaminas;
 
         s.vidurkis();
+        s.mediana();
+
+        std::cout << "Spausdinti pagal mediana (m) ar vidurki (v)? ";
+        char c; in >> c;
+        Studentas::setModeMediana(c == 'm' || c == 'M');
         return in;
     }
 
     // Isvedimas (perdengtas cout)
-    friend ostream& operator<<(ostream& out, const Studentas& s) {
-        out << "Vardas  " << "Pavarde  " << " Galutinis (vid.)"
-            << s.vardas << " " << s.pavarde << s.rezultatas;
+    friend std::ostream& operator<<(std::ostream& out, const Studentas& s) {
+        out << std::left << std::setw(13) << s.vardas
+            << std::setw(13) << s.pavarde
+            << std::fixed << std::setprecision(2)
+            << (naudotiMediana_ ? s.rezultatasM : s.rezultatasV);
         return out;
     }
 };
 
+bool Studentas::naudotiMediana_ = false;
+
 int main()
 {
     Studentas st;
-    std::cin >> st;   
-    std::cout << st;  
+    std::cin >> st;                 
+    Studentas::printHeader(std::cout);
+    std::cout << st << "\n";
+
 }
