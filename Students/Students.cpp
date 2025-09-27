e4d5171e7bc2b68b0d2195cd04c91b8bce9971a3
Laimonas-Vasiliauskas <232161150+Laimonas-Vasiliauskas@users.noreply.github.com>
2025-09-27 09:58:09 +0200

    new changes

--- Students/Students.cpp
+++ Students/Students.cpp
@@ -3,16 +3,23 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <random>
 #include <iomanip>
 
-using std::cout;
 using std::string;
 using std::vector;
 using std::accumulate;
 using std::istream;
 using std::ostream;
+using std::sort;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::stoi;
+using std::copy;
 
 class Studentas {
+private:
     string vardas;
     string pavarde;
     vector<int> pazymiai;
@@ -24,7 +31,7 @@ class Studentas {
 
     void vidurkis() {
         if (!pazymiai.empty()) {
-            double avg = std::accumulate(pazymiai.begin(), pazymiai.end(), 0.0) / pazymiai.size();
+            double avg = accumulate(pazymiai.begin(), pazymiai.end(), 0.0) / pazymiai.size();
             rezultatasV = avg * 0.4 + egzaminas * 0.6;
         }
         else {
@@ -33,17 +40,19 @@ class Studentas {
     }
     void mediana() {
         if (!pazymiai.empty()) {
-            std::vector<int> t = pazymiai;
-            std::sort(t.begin(), t.end());
+            vector<int> t = pazymiai;
+            sort(t.begin(), t.end());
             double med = (t.size() % 2 == 0) ? (t[t.size() / 2 - 1] + t[t.size() / 2]) / 2.0 : t[t.size() / 2];
             rezultatasM = med * 0.4 + egzaminas * 0.6;
         }
         else {
             rezultatasM = egzaminas * 0.6;
         }
-    }
 
+    }
 public:
+    static void setModeMediana(bool m) { naudotiMediana_ = m; }
+
     string getVardas() const {
         return vardas;
     }
@@ -62,38 +71,38 @@ public:
     double getRezultatasM() const {
         return rezultatasM;
     }
-    static void setModeMediana(bool m) { naudotiMediana_ = m; }  
-   
-    static void printHeader(std::ostream& out) {
-        out << std::left << std::setw(13) << "Vardas"
-            << std::setw(13) << "Pavarde"
-            << (naudotiMediana_ ? "Galutinis (Med.)" : "Galutinis (Vid.)") << "\n"
-            << "------------------------------------------------\n";
-    }
 
     // Default constructor
-    Studentas() {
+    Studentas() = default;
 
-    }
     Studentas(string v, string p, vector<int>paz, int egzas) {
         vardas = v;
         pavarde = p;
         pazymiai = paz;
         egzaminas = egzas;
-    }
-
-    // priskyrimo-kopijavimo operatorius
-    Studentas& operator=(const Studentas& other) {
-        if (this == &other) return *this; 
-        vardas = other.vardas;
-        pavarde = other.pavarde;
-        return *this;
+        vidurkis();
+        mediana();
     }
     // kopijavimo konstruktorius
     Studentas(const Studentas& other) {
         vardas = other.vardas;
         pavarde = other.pavarde;
+        copy(other.pazymiai.begin(), other.pazymiai.end(), std::back_inserter(pazymiai));
+        egzaminas = other.egzaminas;
     }
+    //kopijavimo ir priskyrimo kontruktorius
+    Studentas& operator= (const Studentas& other) {  
+        if (this == &other) return *this;
+        else {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            copy(other.pazymiai.begin(), other.pazymiai.end(), std::back_inserter(pazymiai));
+            egzaminas = other.egzaminas;
+            rezultatasV = other.rezultatasV;
+            rezultatasM = other.rezultatasM;
+            return *this;
+            }
+        }
     // destruktorius
     ~Studentas() {
         vardas.clear();
@@ -102,51 +111,80 @@ public:
         egzaminas = 0;
         rezultatasV = 0;
         rezultatasM = 0;
-    };
+    }
     // operatorių perkrovimas 
     friend istream& operator>>(istream& in, Studentas& s) {
-        cout << "Iveskite varda ir pavarde: ";
         in >> s.vardas >> s.pavarde;
-        cout << "Iveskite pazymius" << std::endl;
-        cout << "Baige vesti pazymius paspauskite x ir tada Enter klavisa" << std::endl;
 
+        s.pazymiai.clear();
         string input;
         while (true) {
             in >> input;
-            if (input == "x") break;
-            s.pazymiai.push_back(std::stoi(input));
+            if (input == "x" || input == "X") break;
+            s.pazymiai.push_back(stoi(input));
         }
-        
-        cout << "Iveskite egzamino pazymi: ";
         in >> s.egzaminas;
-
         s.vidurkis();
         s.mediana();
 
-        std::cout << "Spausdinti pagal mediana (m) ar vidurki (v)? ";
-        char c; in >> c;
-        Studentas::setModeMediana(c == 'm' || c == 'M');
         return in;
     }
-
     // Isvedimas (perdengtas cout)
-    friend std::ostream& operator<<(std::ostream& out, const Studentas& s) {
-        out << std::left << std::setw(13) 
-            << s.vardas << std::setw(13) 
-            << s.pavarde << std::fixed 
-            << std::setprecision(2) 
-            << (naudotiMediana_ ? s.rezultatasM : s.rezultatasV);
+    friend ostream& operator<<(ostream& out, const Studentas& s) {
+        
+        double gal = Studentas::naudotiMediana_ ? s.rezultatasM : s.rezultatasV;
+
+        out << s.pavarde << ' ' << s.vardas << ' '
+            << std::fixed << std::setprecision(2) << gal;
+
         return out;
     }
+    // atsitiktiniu skaiciu generavimas
+    void randomize(size_t n, unsigned seed = std::random_device{}()) {
+        std::mt19937 rng(seed);
+        std::uniform_int_distribution<int> d(1, 10);
+        pazymiai.resize(n);
+        for (int& x : pazymiai) x = d(rng);
+        egzaminas = d(rng);
+        vidurkis();
+        mediana();
+    }
 };
-
 bool Studentas::naudotiMediana_ = false;
 
-int main()
-{
-    Studentas st = Studentas();
-    std::cin >> st;                 
-    Studentas::printHeader(std::cout);
-    std::cout << st << "\n";
+int main() {
+    Studentas s;
 
+    cout << "Ar sugeneruoti pazymius atsitiktine tvarka? [T/N]: ";
+    char g;
+    cin >> g;
+
+    if (g == 't' || g == 'T') {
+        string v, p;
+        cout << "Iveskite varda ir pavarde: ";
+        cin >> v >> p;
+
+        cout << "Kiek pazymiu sugeneruoti (1..10)? ";
+        int n;
+        cin >> n;
+
+        s = Studentas(v, p, {}, 0);
+        s.randomize(n);
+    }
+    else {
+        cout << "Iveskite: Vardas Pavarde Pazymius, baige vesti pazymius iveskite raide x ir spauskite Enter, tada egzamino pazymi" << endl;
+        cin >> s; 
+    }
+
+    cout << "Pasirinkite rezultata: [m]ediana arba [v]idurkis: ";
+    char c;
+    cin >> c;
+    Studentas::setModeMediana(c == 'm' || c == 'M');
+
+    cout << "Pavarde " << "Vardas "
+        << ((c == 'm' || c == 'M') ? "Galutinis (Med.)" : "Galutinis (Vid.)") << endl;
+    cout << "----------------------------------------------" << endl;
+    cout << s << endl;
+
+    return 0;
 }
